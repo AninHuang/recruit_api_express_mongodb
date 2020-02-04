2d9a4d29390a2b50b9a89dce5a3802581dfa31fa
AninHuang <lifeanne19904@gmail.com>
2020-02-04 23:33:10 +0800

    Add country code to the route

--- routes/openings.js
+++ routes/openings.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { getOpenings, getOpening, createOpening, updateOpening, deleteOpening, getOpeningsInRadius } = require('../controllers/openings');
 const router = express.Router();
-
-router.route('/radius/:zipcode/:distance').get(getOpeningsInRadius);
-
+ 
+router.route('/radius/:country/:zipcode/:distance').get(getOpeningsInRadius);
+ 
 router.route('/').get(getOpenings).post(createOpening);
-
+ 
 router.route('/:id').get(getOpening).put(updateOpening).delete(deleteOpening);
-
+ 
 module.exports = router;
